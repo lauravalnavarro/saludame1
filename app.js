58d3f286f48d50b194551368a2d3a341924bb0cc
Laura <lvna.95@gmail.com>
2021-03-25 20:49:27 -0500

    saludame2

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  if(req.query.nombre != undefined){
-    res.send(`<h1>Hola ${req.query.nombre}!</h1>`);
+app.get('/makers/:name', (req, res) => {
+  let name = req.params.name;
+  let nameCapit = name.charAt(0).toUpperCase() + name.slice(1);
+  
+  if(nameCapit != undefined){
+    res.send(`<h1>Hola ${nameCapit}!</h1>`);
   }else{
     res.send(`<h1>Hola desconocido!</h1>`);
   }
